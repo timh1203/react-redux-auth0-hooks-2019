522e125d9f2e6754a168b3a9b03b7d4e7aa209ab
Timothy H <timh1203@gmail.com>
2019-12-19 18:31:13 -0600

    Add useEffect()

--- src/hooks/HooksContainer1.js
+++ src/hooks/HooksContainer1.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const HooksContainer1 = () => {
   // HOOKS METHOD 1
   const [stateValue, setValue] = useState(0)
+  const [useEffectValue, setUseEffectValue] = useState(null)
 
   // HOOKS METHOD 2
   // const stateValue = useState(0)[0]
   // const setValue = useState(0)[1]
+
+  useEffect(() => {
+    // Simulate API request with setTimeout
+    // Inserting `stateValue` in the array means that anytime this value is changed,
+    // The useEffect function will run again, essentially back to "useEffect working"
+    setTimeout(() => setUseEffectValue("useEffect working"), 3000)
+  }, [stateValue])
 
   const incrementValue = () => {
     setValue(stateValue + 1)
@@ -16,15 +24,22 @@ const HooksContainer1 = () => {
     setValue(stateValue - 1)
   }
 
+  const changeUseEffectValue = () => {
+    setUseEffectValue("useEffect was changed")
+  }
+
   return (
     <div>
-      HooksContainer1
-      <br />
-      <button onClick={() => incrementValue()}>Increment</button>
-      <button onClick={() => decrementValue()}>Decrement</button>
+      <p>HooksContainer1</p>
+      <div>
+        <span>Local State: {stateValue}</span>
+        <button onClick={() => incrementValue()}>Increment</button>
+        <button onClick={() => decrementValue()}>Decrement</button>
+      </div>
       <br />
       <div>
-        <p>Local State: {stateValue}</p>
+        <span>{useEffectValue || 'Making API call...'}</span>
+        <button onClick={() => changeUseEffectValue()}>Change UseEffect Value</button>
       </div>
     </div>
   )
